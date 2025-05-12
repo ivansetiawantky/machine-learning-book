9f6736d00c8d7d321432dcc12eab08bf402a478a
Ivan Setiawan <ivan@arcadia.co.jp>
2025-05-12 14:51:23 +0900

    Change float_ to float64 for numpy2.

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -1,45 +1,50 @@
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: pymlbook
+#     language: python
+#     name: python3
+# ---
 
-
-import sys
-from python_environment_check import check_packages
-import numpy as np
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-
+# %% [markdown]
 # # Machine Learning with PyTorch and Scikit-Learn  
 # # -- Code Examples
 
+# %% [markdown]
 # ## Package version checks
 
+# %% [markdown]
 # Add folder to path in order to load from the check_packages.py script:
 
+# %%
+import sys
 
+sys.path.insert(0, "..")
 
-sys.path.insert(0, '..')
-
-
+# %% [markdown]
 # Check recommended package versions:
 
+# %%
+from python_environment_check import check_packages
 
 
-
-
-d = {
-    'numpy': '1.21.2',
-    'matplotlib': '3.4.3',
-    'pandas': '1.3.2'
-}
+d = {"numpy": "1.21.2", "matplotlib": "3.4.3", "pandas": "1.3.2"}
 check_packages(d)
 
-
+# %% [markdown]
 # # Chapter 2 - Training Machine Learning Algorithms for Classification
 
+# %% [markdown]
 # ### Overview
-# 
+#
 
+# %% [markdown]
 # - [Artificial neurons – a brief glimpse into the early history of machine learning](#Artificial-neurons-a-brief-glimpse-into-the-early-history-of-machine-learning)
 #     - [The formal definition of an artificial neuron](#The-formal-definition-of-an-artificial-neuron)
 #     - [The perceptron learning rule](#The-perceptron-learning-rule)
@@ -53,40 +58,46 @@ check_packages(d)
 #     - [Large scale machine learning and stochastic gradient descent](#Large-scale-machine-learning-and-stochastic-gradient-descent)
 # - [Summary](#Summary)
 
+# %% [markdown]
+# <br>
+# <br>
 
+# %%
+from IPython.display import Image
 
-
-
-
+# %% [markdown]
 # # Artificial neurons - a brief glimpse into the early history of machine learning
 
+# %%
+Image(filename="./figures/02_01.png", width=500)
 
-
-
-
+# %% [markdown]
 # ## The formal definition of an artificial neuron
 
+# %%
+Image(filename="./figures/02_02.png", width=500)
 
-
-
-
+# %% [markdown]
 # ## The perceptron learning rule
 
+# %%
+Image(filename="./figures/02_03.png", width=600)
 
+# %%
+Image(filename="./figures/02_04.png", width=600)
 
+# %% [markdown]
+# <br>
+# <br>
 
-
-
-
-
-
-
+# %% [markdown]
 # # Implementing a perceptron learning algorithm in Python
 
+# %% [markdown]
 # ## An object-oriented perceptron API
 
-
-
+# %%
+import numpy as np
 
 
 class Perceptron:
@@ -112,6 +123,7 @@ class Perceptron:
       Number of misclassifications (updates) in each epoch.
 
     """
+
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
@@ -135,8 +147,7 @@ class Perceptron:
         """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.b_ = np.float_(0.)
-        
+        self.b_ = np.float64(0.0)
         self.errors_ = []
 
         for _ in range(self.n_iter):
@@ -158,45 +169,51 @@ class Perceptron:
         return np.where(self.net_input(X) >= 0.0, 1, 0)
 
 
-
-
+# %%
 v1 = np.array([1, 2, 3])
 v2 = 0.5 * v1
 np.arccos(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
 
+# %% [markdown]
+# <br>
+# <br>
 
-
+# %% [markdown]
 # ## Training a perceptron model on the Iris dataset
 
+# %% [markdown]
 # ...
 
+# %% [markdown]
 # ### Reading-in the Iris data
 
-
-
+# %%
+import os
+import pandas as pd
 
 try:
-    s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-    print('From URL:', s)
-    df = pd.read_csv(s,
-                     header=None,
-                     encoding='utf-8')
-    
+    s = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    print("From URL:", s)
+    df = pd.read_csv(s, header=None, encoding="utf-8")
+
 except HTTPError:
-    s = 'iris.data'
-    print('From local Iris path:', s)
-    df = pd.read_csv(s,
-                     header=None,
-                     encoding='utf-8')
-    
+    s = "iris.data"
+    print("From local Iris path:", s)
+    df = pd.read_csv(s, header=None, encoding="utf-8")
+
 df.tail()
 
+# %% [markdown]
+# <br>
+# <br>
 
-
+# %% [markdown]
 # ### Plotting the Iris data
 
-
-
+# %%
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
 
 # select setosa and versicolor
 y = df.iloc[0:100, 4].values
@@ -218,43 +235,49 @@ plt.legend(loc='upper left')
 # plt.savefig('images/02_06.png', dpi=300)
 plt.show()
 
+# %% [markdown]
+# <br>
+# <br>
 
-
+# %% [markdown]
 # ### Training the perceptron model
 
-
-
+# %%
 ppn = Perceptron(eta=0.1, n_iter=10)
 
 ppn.fit(X, y)
 
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of updates')
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Number of updates")
 
 # plt.savefig('images/02_07.png', dpi=300)
 plt.show()
 
+# %% [markdown]
+# <br>
+# <br>
 
-
+# %% [markdown]
 # ### A function for plotting decision regions
 
-
-
+# %%
+from matplotlib.colors import ListedColormap
 
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
 
     # setup marker generator and color map
-    markers = ('o', 's', '^', 'v', '<')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
+    markers = ("o", "s", "^", "v", "<")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[: len(np.unique(y))])
 
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution)
+    )
     lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     lab = lab.reshape(xx1.shape)
     plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
@@ -263,47 +286,55 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
 
     # plot class examples
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], 
-                    y=X[y == cl, 1],
-                    alpha=0.8, 
-                    c=colors[idx],
-                    marker=markers[idx], 
-                    label=f'Class {cl}', 
-                    edgecolor='black')
+        plt.scatter(
+            x=X[y == cl, 0],
+            y=X[y == cl, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=markers[idx],
+            label=f"Class {cl}",
+            edgecolor="black",
+        )
 
 
-
-
+# %%
 plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel('Sepal length [cm]')
-plt.ylabel('Petal length [cm]')
-plt.legend(loc='upper left')
+plt.xlabel("Sepal length [cm]")
+plt.ylabel("Petal length [cm]")
+plt.legend(loc="upper left")
 
 
-#plt.savefig('images/02_08.png', dpi=300)
+# plt.savefig('images/02_08.png', dpi=300)
 plt.show()
 
+# %% [markdown]
+# <br>
+# <br>
 
-
+# %% [markdown]
 # # Adaptive linear neurons and the convergence of learning
 
+# %% [markdown]
 # ...
 
+# %% [markdown]
 # ## Minimizing cost functions with gradient descent
 
+# %%
+Image(filename="./figures/02_09.png", width=600)
+
+# %%
+Image(filename="./figures/02_10.png", width=500)
 
 
+# %% [markdown]
+# <br>
+# <br>
 
-
-
-
-
-
-
+# %% [markdown]
 # ## Implementing an adaptive linear neuron in Python
 
-
-
+# %%
 class AdalineGD:
     """ADAptive LInear NEuron classifier.
 
@@ -328,13 +359,14 @@ class AdalineGD:
       Mean squared eror loss function values in each epoch.
 
     """
+
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, y):
-        """ Fit training data.
+        """Fit training data.
 
         Parameters
         ----------
@@ -351,7 +383,7 @@ class AdalineGD:
         """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.b_ = np.float_(0.)
+        self.b_ = np.float64(0.0)
         self.losses_ = []
 
         for i in range(self.n_iter):
@@ -359,16 +391,16 @@ class AdalineGD:
             # Please note that the "activation" method has no effect
             # in the code since it is simply an identity function. We
             # could write `output = self.net_input(X)` directly instead.
-            # The purpose of the activation is more conceptual, i.e.,  
-            # in the case of logistic regression (as we will see later), 
+            # The purpose of the activation is more conceptual, i.e.,
+            # in the case of logistic regression (as we will see later),
             # we could change it to
             # a sigmoid function to implement a logistic regression classifier.
             output = self.activation(net_input)
-            errors = (y - output)
-            
-            #for w_j in range(self.w_.shape[0]):
+            errors = y - output
+
+            # for w_j in range(self.w_.shape[0]):
             #    self.w_[w_j] += self.eta * (2.0 * (X[:, w_j]*errors)).mean()
-            
+
             self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
             self.b_ += self.eta * 2.0 * errors.mean()
             loss = (errors**2).mean()
@@ -388,74 +420,77 @@ class AdalineGD:
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
 
 
-
-
+# %%
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
 ada1 = AdalineGD(n_iter=15, eta=0.1).fit(X, y)
-ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Mean squared error)')
-ax[0].set_title('Adaline - Learning rate 0.1')
+ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker="o")
+ax[0].set_xlabel("Epochs")
+ax[0].set_ylabel("log(Mean squared error)")
+ax[0].set_title("Adaline - Learning rate 0.1")
 
 ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.losses_) + 1), ada2.losses_, marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('Mean squared error')
-ax[1].set_title('Adaline - Learning rate 0.0001')
+ax[1].plot(range(1, len(ada2.losses_) + 1), ada2.losses_, marker="o")
+ax[1].set_xlabel("Epochs")
+ax[1].set_ylabel("Mean squared error")
+ax[1].set_title("Adaline - Learning rate 0.0001")
 
 # plt.savefig('images/02_11.png', dpi=300)
 plt.show()
 
+# %% [markdown]
+# <br>
+# <br>
 
+# %%
+Image(filename="./figures/02_12.png", width=700)
 
+# %% [markdown]
+# <br>
+# <br>
 
-
-
-
-
+# %% [markdown]
 # ## Improving gradient descent through feature scaling
 
+# %%
+Image(filename="./figures/02_13.png", width=700)
 
-
-
-
-
-
+# %%
 # standardize features
 X_std = np.copy(X)
 X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
 X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
 
-
-
-
+# %%
 ada_gd = AdalineGD(n_iter=20, eta=0.5)
 ada_gd.fit(X_std, y)
 
 plot_decision_regions(X_std, y, classifier=ada_gd)
-plt.title('Adaline - Gradient descent')
-plt.xlabel('Sepal length [standardized]')
-plt.ylabel('Petal length [standardized]')
-plt.legend(loc='upper left')
+plt.title("Adaline - Gradient descent")
+plt.xlabel("Sepal length [standardized]")
+plt.ylabel("Petal length [standardized]")
+plt.legend(loc="upper left")
 plt.tight_layout()
-#plt.savefig('images/02_14_1.png', dpi=300)
+# plt.savefig('images/02_14_1.png', dpi=300)
 plt.show()
 
-plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Mean squared error')
+plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Mean squared error")
 
 plt.tight_layout()
-#plt.savefig('images/02_14_2.png', dpi=300)
+# plt.savefig('images/02_14_2.png', dpi=300)
 plt.show()
 
 
+# %% [markdown]
+# <br>
+# <br>
 
+# %% [markdown]
 # ## Large scale machine learning and stochastic gradient descent
 
-
-
+# %%
 class AdalineSGD:
     """ADAptive LInear NEuron classifier.
 
@@ -482,17 +517,18 @@ class AdalineSGD:
       Mean squared error loss function value averaged over all
       training examples in each epoch.
 
-        
+
     """
+
     def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
         self.eta = eta
         self.n_iter = n_iter
         self.w_initialized = False
         self.shuffle = shuffle
         self.random_state = random_state
-        
+
     def fit(self, X, y):
-        """ Fit training data.
+        """Fit training data.
 
         Parameters
         ----------
@@ -534,23 +570,23 @@ class AdalineSGD:
         """Shuffle training data"""
         r = self.rgen.permutation(len(y))
         return X[r], y[r]
-    
+
     def _initialize_weights(self, m):
         """Initialize weights to small random numbers"""
         self.rgen = np.random.RandomState(self.random_state)
         self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
-        self.b_ = np.float_(0.)
+        self.b_ = np.float64(0.0)
         self.w_initialized = True
-        
+
     def _update_weights(self, xi, target):
         """Apply Adaline learning rule to update the weights"""
         output = self.activation(self.net_input(xi))
-        error = (target - output)
+        error = target - output
         self.w_ += self.eta * 2.0 * xi * (error)
         self.b_ += self.eta * 2.0 * error
         loss = error**2
         return loss
-    
+
     def net_input(self, X):
         """Calculate net input"""
         return np.dot(X, self.w_) + self.b_
@@ -564,48 +600,47 @@ class AdalineSGD:
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
 
 
-
-
+# %%
 ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
 ada_sgd.fit(X_std, y)
 
 plot_decision_regions(X_std, y, classifier=ada_sgd)
-plt.title('Adaline - Stochastic gradient descent')
-plt.xlabel('Sepal length [standardized]')
-plt.ylabel('Petal length [standardized]')
-plt.legend(loc='upper left')
+plt.title("Adaline - Stochastic gradient descent")
+plt.xlabel("Sepal length [standardized]")
+plt.ylabel("Petal length [standardized]")
+plt.legend(loc="upper left")
 
 plt.tight_layout()
-plt.savefig('figures/02_15_1.png', dpi=300)
+# plt.savefig("figures/02_15_1.png", dpi=300)
 plt.show()
 
-plt.plot(range(1, len(ada_sgd.losses_) + 1), ada_sgd.losses_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Average loss')
+plt.plot(range(1, len(ada_sgd.losses_) + 1), ada_sgd.losses_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Average loss")
 
-plt.savefig('figures/02_15_2.png', dpi=300)
+# plt.savefig("figures/02_15_2.png", dpi=300)
 plt.show()
 
-
-
-
+# %%
 ada_sgd.partial_fit(X_std[0, :], y[0])
 
+# %% [markdown]
+# <br>
+# <br>
 
-
+# %% [markdown]
 # # Summary
 
+# %% [markdown]
 # ...
 
+# %% [markdown]
 # --- 
-# 
+#
 # Readers may ignore the following cell
 
-
-
-
-
-
-
-
-
+# %%
+# # ! python ../.convert_notebook_to_script.py --input ch02.ipynb --output ch02.py
+#
+# Use jupytext to convert ipynb to py, as below. OR just use vscode Export
+# ! jupytext --to py:percent ch02.ipynb
